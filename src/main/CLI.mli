58b50e03f7a37d1789c28db93c21caf6fcfc1cc9
pad <yoann.padioleau@gmail.com>
2025-09-09 12:37:21 +0200

    make sync

--- src/main/CLI.mli
+++ src/main/CLI.mli
@@ -1,4 +1,9 @@
+(*s: CLI.mli *)
+(*s: type [[CLI.caps]] *)
 type caps = < Cap.no_caps >
-
+(*e: type [[CLI.caps]] *)
+(*s: signature [[CLI.main]] *)
 (* entry point (can also raise Exit.ExitCode) *)
 val main : < caps ; .. > -> string array -> Exit.t
+(*e: signature [[CLI.main]] *)
+(*e: CLI.mli *)
